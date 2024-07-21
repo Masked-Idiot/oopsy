9814f22daee60e8f463be7dc9da8880e37fdcb9b
Masked-Idiot <blaze12768@gmail.com>
2024-07-21 11:28:55 -0400

    examples

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 require("oopsy")
-local person = class({
+local person = class({},{
 	init = function(self, args)
         self.name = args[1]
         self.age = args[2]
@@ -8,6 +8,17 @@ local person = class({
 		print("Hello, I am " .. self.name .. ", and I am " .. tostring(self.age) .. " years old!")
 	end
 })
+local worker = class({person},{
+	init = function(self, args)
+		person.init(self, args)
+		self.salary = args[3]
+		self.money = args[4]
+	end,
+	work = function(self)
+		self.money = self.money + self.salary
+	end
+})
 
+--local brink = worker:new({"Mark", 50, 20, 300})
 local liam = person:new({"Liam", 13})
 liam:describe()
